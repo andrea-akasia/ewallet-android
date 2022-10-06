c7f7d90bed1e88e3137411f46588aac3752b1810
adiandrea <dekz.itfz@gmail.com>
2022-10-06 10:46:31 +0800

    implement share receipt in detail transaction

--- app/src/main/java/com/mobile/ewallet/feature/home/TransactionDetailActivity.kt
+++ app/src/main/java/com/mobile/ewallet/feature/home/TransactionDetailActivity.kt
@@ -1,6 +1,12 @@
 package com.mobile.ewallet.feature.home
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -9,6 +15,7 @@ import com.mobile.ewallet.base.BaseActivity
 import com.mobile.ewallet.databinding.ActivityDetailTransactionBinding
 import com.mobile.ewallet.util.GlideApp
 import com.mobile.ewallet.util.formatToCurrency
+
 
 class TransactionDetailActivity: BaseActivity<HomeViewModel>() {
 
@@ -50,10 +57,37 @@ class TransactionDetailActivity: BaseActivity<HomeViewModel>() {
             binding.amount.text = it.amount.formatToCurrency()
             binding.admin.text = if(it.adminFeeText == "0") "Gratis" else it.adminFeeText.formatToCurrency()
             binding.total.text = it.total.formatToCurrency()
+
+            binding.btnShare.setOnClickListener {
+                share(screenShot(binding.root.rootView))
+            }
         }
 
         viewModel.warningMessage.observe(this){
             Toast.makeText(this, "it", Toast.LENGTH_SHORT).show()
         }
     }
+
+    private fun screenShot(view: View): Bitmap {
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        view.draw(canvas)
+        return bitmap
+    }
+
+    private fun share(bitmap: Bitmap) {
+        val pathofBmp = MediaStore.Images.Media.insertImage(
+            contentResolver,
+            bitmap, "title", null
+        )
+        val uri: Uri = Uri.parse(pathofBmp)
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.type = "image/*"
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Share receipt")
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "")
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+        startActivity(Intent.createChooser(shareIntent, "Share Receipt"))
+    }
+
+
 }
